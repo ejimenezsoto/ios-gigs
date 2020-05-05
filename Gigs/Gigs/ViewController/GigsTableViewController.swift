606d9fe9674c0191e8f462f4e66f87c159097e7b
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-05-05 13:27:42 -0700

    Working on Login View Controller

--- Gigs/Gigs/ViewController/GigsTableViewController.swift
+++ Gigs/Gigs/ViewController/GigsTableViewController.swift
@@ -12,13 +12,9 @@ class GigsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+
+    
 
     // MARK: - Table view data source
 
@@ -31,6 +27,9 @@ class GigsTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "LoginModalSegue" {
+        guard let loginVC = segue.destination as? LoginViewController else { return }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- Gigs/Gigs/ViewController/LoginViewController.swift
+++ Gigs/Gigs/ViewController/LoginViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    enum LoginType{
+        case signUp
+        case signIn
+    }
 
+
+    
+    @IBOutlet weak var loginSignupSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
